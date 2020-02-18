0658defa8f36f52aa5d3452c9f738b893486eb7f
DiepPhuc <56315134+DiepPhuc@users.noreply.github.com>
2020-02-19 02:46:40 +0700

    Add files via upload

---
+++ Week1/task4.js
@@ -0,0 +1,33 @@
+const students = [
+    {
+      name: 'Nam',
+      age: 24,
+      gender: 'male',
+    },
+    {
+      name: 'Mai',
+      age: 22,
+      gender: 'female',
+    },
+    {
+      name: 'Trang',
+      age: 23,
+      gender: 'female',
+    },
+    {
+      name: 'An',
+      age: 20,
+      gender: 'male',
+    },
+    {
+      name: 'Thien',
+      age: 27,
+      gender: 'male',
+    },
+  ];
+  const convertArray = students.map(function(obj){
+    let array = {};
+    array = obj.name;
+    return array;
+  });
+  console.log(convertArray);
